a807c9ac92d3f58756434dd9d56b019400412a50
InSaneDarwin <cds87@live.com>
2017-03-26 08:53:13 -0400

    Initial import

---
+++ Source/version_info.c
@@ -0,0 +1,11 @@
+/*
+ *  version_info.c
+ *  Libm
+ *
+ *  Created by Ian Ollmann on 6/27/07.
+ *  Copyright 2007 Apple Inc. All rights reserved.
+ *
+ */
+
+const char __Libm_version[] = "@(#) " LIBM_VERSION_STRING " " __DATE__ " " __TIME__;
+//const char __Libm_version[] = "@(#) " "2026" " " __DATE__ " " __TIME__;
